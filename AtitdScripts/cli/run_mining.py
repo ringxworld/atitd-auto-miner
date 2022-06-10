39aecb1617cecf4ced5313726bd7a0dba6333596
Shawn Anderson <sea614@mac.com>
2022-06-09 19:19:48 -0700

    fix run script

--- AtitdScripts/cli/run_mining.py
+++ AtitdScripts/cli/run_mining.py
@@ -1,113 +1,17 @@
 import os
 import pytesseract
-import time
+from AtitdScripts.mining.mining import Mining
+import os
+
+import pytesseract
+
+from AtitdScripts.mining.mining import Mining
 
 
 def run(*args, **kwargs):
-    tracker = Tracker()
-    tracker.run(startup, )
-    pass
+    mining = Mining(**kwargs)
+    mining.run()
 
-
-def startup():
-    template_match_click(os.path.join(os.path.dirname(__file__), 'AtitdScripts","images', 'stop_working_this_mine.png'),
-                         {"top": 0, "left": 0, "width": 500, "height": 400})
-
-    time.sleep(2.5)
-
-
-def runnable(mask, downsample):
-    print("Checking for clusters")
-    downsample = mask.copy()
-    for l in range(int(downsample)):
-        downsample = cv2.pyrDown(downsample)
-
-    idx = (downsample > 0)
-    points = np.column_stack(np.nonzero(idx))
-    # weights = np.full((downsample.shape[0], downsample.shape[1]), 255)[idx].ravel().astype(float)
-    weights = downsample[idx].ravel().astype(float)
-
-    db = DBSCAN(eps=float(kwargs.get('eps')),
-                min_samples=int(kwargs.get('min_samples')),
-                metric='euclidean',
-                algorithm='auto')
-    labels = db.fit_predict(points, sample_weight=weights)
-
-    n_clusters = int(np.max(labels)) + 1
-    print(n_clusters)
-    previous_cluster_count = n_clusters
-    if n_clusters != int(kwargs.get('clusters')):
-        count = 0
-        previous_cluster_coordinates = []
-        if kwargs.get('debug'):
-            for l in range(n_clusters):
-                idx = (labels == l)
-                current = points[idx]
-                out = aabb.create_from_points(current)
-                previous_cluster_coordinates.append(out)
-        path = os.path.join(os.path.dirname(__file__), "training_set",
-                            f'{uuid.uuid4()}_badmatch_downsampled.png')
-        print("Didn't match the expected number of clusters. Retrying")
-        if kwargs.get('debug'):
-            cv2.imwrite(path, downsample)
-        time.sleep(1)
-        template_match_click(
-            os.path.join(os.path.dirname(__file__), 'images', 'stop_working_this_mine.png'),
-            {"top": 0, "left": 0, "width": 500, "height": 400})
-
-    if n_clusters == int(kwargs.get('clusters')):
-        print("Found correct number of clusters. Gathering center points to start Mining mini-game")
-        previous_cluster_coordinates = []
-        path = os.path.join(os.path.dirname(__file__), "training_set", f'{uuid.uuid4()}_goodmatch_downsampled.png')
-        cv2.imwrite(path, downsample)
-        for l in range(n_clusters):
-            idx = (labels == l)
-            current = points[idx]
-
-            center = aabb.centre_point(aabb.create_from_points(current))
-            center[0] = center[0] * (2 ** int(kwargs.get('downsample')))
-            center[1] = center[1] * (2 ** int(kwargs.get('downsample')))
-
-            cluster_points.append(center)
-            center_colors.append((tuple(center), tuple(img[int(center[0]), int(center[1]), :].tolist())))
-
-            if kwargs.get('debug'):
-                out = aabb.create_from_points(current)
-                UPSCALE_OFFSET = (2 ** int(kwargs.get('downsample')))
-                cv2.putText(img=tmp,
-                            text=f"Cluster:{l}",
-                            org=(out[0, 1] * UPSCALE_OFFSET,
-                                 out[0, 0] * UPSCALE_OFFSET - 10),
-                            fontFace=cv2.FONT_HERSHEY_PLAIN,
-                            fontScale=0.9,
-                            color=(255, 0, 0),
-                            thickness=2)
-
-                cv2.rectangle(img=tmp,
-                              pt1=(out[0, 1] * UPSCALE_OFFSET,
-                                   out[0, 0] * UPSCALE_OFFSET),
-                              pt2=(out[1, 1] * UPSCALE_OFFSET,
-                                   out[1, 0] * UPSCALE_OFFSET),
-                              color=(255, 0, 0),
-                              thickness=2)
-                cv2.putText(img=img,
-                            text=f"Cluster:{l}",
-                            org=(out[0, 1] * UPSCALE_OFFSET,
-                                 out[0, 0] * UPSCALE_OFFSET - 10),
-                            fontFace=cv2.FONT_HERSHEY_PLAIN,
-                            fontScale=0.9,
-                            color=(255, 0, 0),
-                            thickness=2)
-
-                cv2.rectangle(img=img,
-                              pt1=(out[0, 1] * UPSCALE_OFFSET,
-                                   out[0, 0] * UPSCALE_OFFSET),
-                              pt2=(out[1, 1] * UPSCALE_OFFSET,
-                                   out[1, 0] * UPSCALE_OFFSET),
-                              color=(255, 0, 0),
-                              thickness=2)
-        count = 0
-        running = False
 
 if __name__ == "__main__":
     import argparse
